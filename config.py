8c2f067028edffc4b6296fb9b27c36ae6dbb011d
a794133319 <55074811+a794133319@users.noreply.github.com>
2022-09-04 16:20:01 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# 公众号配置
+# 公众号appId
+app_id = "wxd7b91188e0d19e6e"
+# 公众号appSecret
+app_secret = "e904091c8b94999c23b2fc34e46541a3"
+# 模板消息id
+template_id = "0rLkbyWgnm0O7nXB7p7WQ2pR8IefFyOF6gvOD__DfUk"
+# 接收公众号消息的微信号
+
+# 这是openid
+user = ["oY8CK5oRnCSb5rg_1K_m67arldbk"]
+
+# 信息配置
+# 所在省份
+province = "河北"
+# 所在城市
+city = "邯郸"
+# 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
+birthday = "2002-9-2"
+# 在一起的日子，格式同上
+love_date = "2021-3-8"
