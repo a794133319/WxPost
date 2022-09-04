93bf16cca864e34b13cd19a50bc789f486ee0f11
a794133319 <55074811+a794133319@users.noreply.github.com>
2022-09-04 20:30:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
                     "color": "#87CEEB"
                 },
                 "birthday": {
-                    "value": birth_day,
+                    "value": birthady,
                     "color": "#FF8000"
                 },
                 "zaoan": {
