4c45243cc71d2c51ab939cef0b51f02d5594850c
a794133319 <55074811+a794133319@users.noreply.github.com>
2022-09-04 19:34:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ app_id = "wxd7b91188e0d19e6e"
 # 公众号appSecret
 app_secret = "e904091c8b94999c23b2fc34e46541a3"
 # 模板消息id
-template_id = "uA2ysyshcNIfk10wEMuMvFRQY191DFmTv_mJTeZKWXQ"
+template_id = "ApTl_66cBhwPfEVlDvTY27R-wARrpmbEqgZmBfqnf5Y"
 # 接收公众号消息的微信号
 # 这是openid  
 user = ["oY8CK5oRnCSb5rg_1K_m67arldbk","oY8CK5ra80SyqnOvOCOl-9bKUO7M"]
